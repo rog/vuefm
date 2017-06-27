b3351cbe303cb5470c1b7e77b4b76754b1260dec
Rogelio Alberto <i@rog.mx>
2017-06-27 12:40:00 +0000

    Artist Component

---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+li
+  h2: a(:href="artist.url" target="_blank") {{ artist.name }}
+  img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: ['artist']
+}
+</script>
+
+<style lang="scss">
+li {
+  display: block;
+  margin: 10px 0;
+}
+</style>
